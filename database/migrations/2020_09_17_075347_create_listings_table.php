703c7b354927ebfcebafbca47240cbfa50a99a13
hannes <hannes@agilearchitects.se>
2020-09-17 15:29:49 +0200

    Adds model and migration for listing

---
+++ database/migrations/2020_09_17_075347_create_listings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateListingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('listings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->integer('price');
+            $table->string('address');
+            $table->string('contact_name');
+            $table->string('contact_email');
+            $table->string('contact_phone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('listings');
+    }
+}
